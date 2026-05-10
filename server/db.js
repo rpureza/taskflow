0fe5348eff92f7ce4c346107d0baea91ce7afae1
Revi Pureza <sagepureza@gmail.com>
2026-05-10 22:00:29 +1200

    Add environment variables support

--- server/db.js
+++ server/db.js
@@ -1,24 +1,18 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "1234",
-    database: "todoapp",
-
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "1234",
+  database: process.env.DB_NAME || "todoapp",
 });
 
 db.connect((err) => {
-    
-    if (err) {
-        console.error("MySQL connection error:", err.message);
-        process.exit(1);
-    }
-
-    console.log("MySQL Connected");
-
-
-
+  if (err) {
+    console.error("MySQL connection error:", err.message);
+    process.exit(1);
+  }
+  console.log("✅ MySQL Connected");
 });
 
 module.exports = db;
--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.get("/", (req,res) =>{
     res.json({message: "Taskflow API is running"});
 });
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () =>{
 
